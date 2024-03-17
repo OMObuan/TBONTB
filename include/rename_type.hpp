572d7051813504515e7244ce3dd955126455a5a4
OMObuan <dubbyzhong@163.com>
2024-03-17 13:49:03 +0800

    second commit: remove ".cache/"

--- include/rename_type.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright [2024] <OMObuan>
-#include <cstddef>
-#include <cstdint>
-
-using i32 = int32_t;
-using usize = std::size_t;
-using f64 = double;
-using f32 = float;
--- src/main.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright [2024] <OMObuan>
-using i32 = int;
-
-i32 main(i32, char *[]) {}
